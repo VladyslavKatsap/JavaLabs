71a735eed01b5879b7b8f930ca23ade7c277630a
Vladyslav <katsap.vladyslav.clg@chnu.edu.ua>
2023-12-04 15:20:03 +0200

    lab3 змінено

--- src/main/java/lab3/Main.java
+++ src/main/java/lab3/Main.java
@@ -7,8 +7,7 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
-public class StreamTest {
-
+public class Main {
     public static void main(String[] args) {
         Athlete athlete1 = new Athlete.AthleteBuilder("Oleg")
                 .dateOfBirth(LocalDate.of(2003, 1, 1))
@@ -17,15 +16,13 @@ public class StreamTest {
                 .dateOfBirth(LocalDate.of(2001, 3, 28))
                 .build();
         Athlete athlete3 = new Athlete.AthleteBuilder("Pavlo")
-                .dateOfBirth(LocalDate.of(2000, 2, 14))
+                .dateOfBirth(LocalDate.of(2005, 2, 14))
                 .build();
         // Enroll the student in subjects and assign grades
 
         Competition bp = new Competition("Bench press", 93);
         Competition deadlift = new Competition("Deadlift", 100);
         Competition squat = new Competition("Squat", 93);
-
-
         // Додавання студентів до списку
         List<Athlete> athletes = new ArrayList<>();
         athletes.add(athlete1);
@@ -36,24 +33,26 @@ public class StreamTest {
         System.out.println("Unsorted students:");
         athletes.forEach(athlete -> System.out.println(athlete.getName() + " - " + athlete.getDateOfBirth()));
 
-
-        // Create an instance of SchoolServiceStream
-        ZmagServiceStream zmagServiceStream = new ZmagServiceStream(athletes);
+        // Create an instance of SchoolService1
+        ZmagServices zmagServices = new ZmagServices(athletes);
 
         // Test sorting by name
         System.out.println("Sorted by name:");
-        zmagServiceStream.sortAthleteByName().forEach(athlete -> System.out.println(athlete.getName()));
+        zmagServices.sortAthleteByName().forEach(athlete -> System.out.println(athlete.getName()));
 
         // Test sorting by date of birth
         System.out.println("\nSorted by date of birth:");
-        zmagServiceStream.sortAthleteByName().forEach(athlete -> System.out.println(athlete.getName()));
+        zmagServices.sortAthleteByDateOfBirth().forEach(athlete -> System.out.println(athlete.getName()));
 
         // Test filtering by part of the name
         System.out.println("\nFiltered by part of the name:");
-        zmagServiceStream.getByPartName("Iv").forEach(athlete -> System.out.println(athlete.getName()));
+        zmagServices.getByPartName("Ol").forEach(athlete -> System.out.println(athlete.getName()));
 
         // Test filtering by current month
         System.out.println("\nFiltered by current month:");
-        zmagServiceStream.getByCurrentMonth().forEach(athlete -> System.out.println(athlete.getName()));
+        zmagServices.getByCurrentMonth().forEach(athlete -> System.out.println(athlete.getName()));
+
+
+
     }
 }
