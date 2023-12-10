97a9ca9f767cff452889a1a23871c36cbf749949
Vladyslav <katsap.vladyslav.clg@chnu.edu.ua>
2023-12-10 23:59:04 +0200

    lab5

--- src/test/java/lab5/CreateTablesTest.java
+++ src/test/java/lab5/CreateTablesTest.java
@@ -12,10 +12,11 @@ public class CreateTablesTest {
     @Test
     public void createTables() {
         try {
-            sqlExecutor.executeQueryWithNoResult("DROP TABLE IF EXISTS lab.athlete;");
-            sqlExecutor.executeQueryWithNoResult("DROP TABLE IF EXISTS lab.competition;");
-            sqlExecutor.executeQueryWithNoResult("DROP TABLE IF EXISTS lab.championship;");
+            sqlExecutor.executeQueryWithNoResult("DROP TABLE IF EXISTS lab.championship_has_position;");
             sqlExecutor.executeQueryWithNoResult("DROP TABLE IF EXISTS lab.position;");
+            sqlExecutor.executeQueryWithNoResult("DROP TABLE IF EXISTS lab.championship;");
+            sqlExecutor.executeQueryWithNoResult("DROP TABLE IF EXISTS lab.competition;");
+            sqlExecutor.executeQueryWithNoResult("DROP TABLE IF EXISTS lab.athlete;");
 
             sqlExecutor.executeQueryWithNoResult("CREATE TABLE athlete(\n" +
                     "`id` INT NOT NULL AUTO_INCREMENT,\n" +
@@ -53,6 +54,7 @@ public class CreateTablesTest {
                     "ON DELETE NO ACTION\n"+
                     "ON UPDATE NO ACTION\n"+
                     ");");
+
             sqlExecutor.executeQueryWithNoResult("CREATE TABLE championship(\n" +
                     "`id` INT NOT NULL AUTO_INCREMENT,\n"+
                     "`title` VARCHAR(80) NOT NULL,\n"+
@@ -60,21 +62,21 @@ public class CreateTablesTest {
                     "UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE\n"+
                     ");");
             sqlExecutor.executeQueryWithNoResult("CREATE TABLE championship_has_position(\n" +
-                    "championship_id INT NOT NULL,\n"+
-                    "position_id INT NOT NULL,\n"+
-                    "position_competition_id INT NOT NULL,\n"+
-                    "position_athlete_id INT NOT NULL,\n"+
-                    "PRIMARY KEY (championship_id, position_id, position_competition_id, position_athlete_id),\n"+
-                    "INDEX fk_championship_has_position_position1_idx (position_id ASC, position_competition_id ASC, position_athlete_id ASC) VISIBLE,\n"+
-                    "INDEX fk_championship_has_position_championship1_idx (championship_id ASC) VISIBLE,\n"+
-                    "CONSTRAINT fk_championship_has_position_championship1\n"+
-                    "FOREIGN KEY (championship_id)\n"+
-                    "REFERENCES mydb.championship (id)\n"+
+                    "`championship_id` INT NOT NULL,\n"+
+                    "`position_id` INT NOT NULL,\n"+
+                    "`position_competition_id` INT NOT NULL,\n"+
+                    "`position_athlete_id` INT NOT NULL,\n"+
+                    "PRIMARY KEY (`championship_id`, `position_id`, `position_competition_id`, `position_athlete_id`),\n"+
+                    "INDEX `fk_championship_has_position_position1_idx` (`position_id` ASC, `position_competition_id` ASC, `position_athlete_id` ASC) VISIBLE,\n"+
+                    "INDEX `fk_championship_has_position_championship1_idx` (`championship_id` ASC) VISIBLE,\n"+
+                    "CONSTRAINT `fk_championship_has_position_championship1`\n"+
+                    "FOREIGN KEY (`championship_id`)\n"+
+                    "REFERENCES `lab`.`championship` (`id`)\n"+
                     "ON DELETE NO ACTION\n"+
                     "ON UPDATE NO ACTION,\n"+
-                    "CONSTRAINT fk_championship_has_position_position1\n"+
-                    "FOREIGN KEY (position_id , position_competition_id , position_athlete_id)\n"+
-                    "REFERENCES mydb.position (id , competition_id , athlete_id)\n"+
+                    "CONSTRAINT `fk_championship_has_position_position1`\n"+
+                    "FOREIGN KEY (`position_id` , `position_competition_id` , `position_athlete_id`)\n"+
+                    "REFERENCES `lab`.`position` (`id` , `competition_id` , `athlete_id`)\n"+
                     "ON DELETE NO ACTION\n"+
                     "ON UPDATE NO ACTION\n"+
                     ");");
